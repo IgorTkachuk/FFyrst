b52cf49b07ba4d14d7dd9e21611f8b7a4dc5a52a
Evgeniy Koval <glggboko@gmail.com>
2021-07-08 15:30:58 +0300

    fixed activation message + started mail service spread

--- backend/src/services/mail-service/mail-service.service.ts
+++ backend/src/services/mail-service/mail-service.service.ts
@@ -1,5 +1,8 @@
 import { host, port, user, pass } from 'config/email.config';
+import { emailType } from '~/common/enums';
 import { Transporter } from 'nodemailer';
+import { Request } from 'express';
+import { getFullUrl } from '~/helpers';
 const pug = require('pug');
 const nodemailer = require('nodemailer');
 
@@ -13,6 +16,16 @@ type mailOptions = {
 class MailService {
   transporter: Transporter;
   constructor() {
+    // this.transporter = nodemailer.createTransport({
+    //   host: 'smtp.ethereal.email',
+    //   port: 587,
+    //   auth: {
+    //       user: 'emma68@ethereal.email',
+    //       pass: 'Qgtk3UCAC4uuqa61eG'
+    //   }
+    // }, {
+    //   from: 'emma68@ethereal.email',
+    // })
     this.transporter = nodemailer.createTransport({
       host,
       port,
@@ -20,28 +33,35 @@ class MailService {
         user,
         pass,
       },
-      secure: port === '465' ? true : false,
+      secure: port === '465',
     });
   }
   //type: check enum emailType  email: example@gmail.com
-  async sendMail(type: any, email: string, options: mailOptions) {
-    await this.transporter.sendMail(
+  async sendMail(type: string, email: string, options: mailOptions):Promise<Transporter> {
+    return this.transporter.sendMail(
       {
         from: user,
         to: email,
         subject: options?.mailTheme,
-        html: pug.compileFile(`${__dirname}/templates/${type}.pug`, {
+        html: pug.compileFile(`${__dirname}/templates/${type}.pug`)({
           ...options?.data,
-        })(),
+        }),
       },
-      function (error: any, info: any) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      },
+      // function (error: any, info: any) {
+      //   if (error) {
+      //     console.log(error);
+      //   } else {
+      //     console.log('Email sent: ' + info.response);
+      //   }
+      // },
     );
+  }
+  async sendActivationMail(_req: Request): Promise<Transporter>{
+    return await this.sendMail(emailType.ACTIVATION, _req.body, {
+      mailTheme: 'Confirm registration',
+      data: {
+        link: getFullUrl(_req.originalUrl),
+    }});
   }
 }
 
