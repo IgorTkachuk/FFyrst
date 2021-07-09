888154d13ac216f2f44718964caad8d88b761df3
Yuriy Lipchey <xaosmmm@gmail.com>
2021-07-09 13:38:52 +0300

    fix jwt config

--- backend/config/jwt.config.js
+++ backend/config/jwt.config.js
@@ -6,12 +6,12 @@ const {
   JWT_EXPIRE_TIME,
   JWT_SECRET,
   JWT_REFRESH_SECRET,
-  REFRESH_EXPIRE_TIME,
+  JWT_REFRESH_EXPIRE_TIME,
 } = process.env;
 
 module.exports = {
   expire: JWT_EXPIRE_TIME,
   secret: JWT_SECRET,
   secretRefresh: JWT_REFRESH_SECRET,
-  expireRefresh: REFRESH_EXPIRE_TIME,
+  expireRefresh: JWT_REFRESH_EXPIRE_TIME,
 };
