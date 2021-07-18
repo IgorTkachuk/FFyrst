5a8e5f609b09404fb1a8071ffc0e945bdb3a7ce9
Igor Tkachuk <I.Tka4yk@gmail.com>
2021-07-18 13:31:20 +0300

    feat: add error handling and file size limit

--- backend/src/api/file-upload/file-upload.api.ts
+++ backend/src/api/file-upload/file-upload.api.ts
@@ -7,11 +7,14 @@ const initFileUploadApi = (apiRouter: Router): Router => {
 
   apiRouter.use(ApiPath.FILE_UPLOAD, fileUploadRouter);
 
-  fileUploadRouter.post(FileUploadApiPath.ROOT, fileUploadService.parser.single('image') ,  (_req, _res) => {
+  fileUploadRouter.post(FileUploadApiPath.ROOT, (_req, _res) => {
+    fileUploadService.getSingleParser('image')(_req, _res, (err: unknown) => {
+      if (err) return _res.status(HttpCode.BAD_REQUEST).json({ error: err });
       _res.status(HttpCode.OK).json({ url: _req.file?.path });
+    });
   });
 
   return fileUploadRouter;
-}
+};
 
 export { initFileUploadApi };
--- backend/src/services/file-upload/file-upload-service.ts
+++ backend/src/services/file-upload/file-upload-service.ts
@@ -1,13 +1,15 @@
 import { cloud_name, api_key, api_secret } from 'config/cloudinary.config';
+import { image_size_limit } from 'config/file-ipload.config';
 import { nanoid } from 'nanoid';
 import { v2 as Cloudinary } from 'cloudinary';
-import { CloudinaryStorage } from 'multer-storage-cloudinary'
+import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer';
+import { RequestHandler } from 'express';
 
 class FileUploadService {
   parser: multer.Multer;
 
-  constructor(){
+  constructor() {
     Cloudinary.config({
       cloud_name,
       api_key,
@@ -25,9 +27,12 @@ class FileUploadService {
       },
     });
 
-    this.parser = multer({ storage });
+    this.parser = multer({ storage, limits: { fileSize: image_size_limit } });
   }
 
+  getSingleParser(fieldName: string): RequestHandler {
+    return this.parser.single(fieldName);
+  }
 }
 
-export {FileUploadService};
+export { FileUploadService };
