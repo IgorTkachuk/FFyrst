4beca5e8fa8f7809f33a24956dbb505b32a689f5
Evgeniy Koval <glggboko@gmail.com>
2021-07-22 20:13:42 +0300

    Create industry instance on backend

--- backend/src/data/repositories/index.ts
+++ backend/src/data/repositories/index.ts
@@ -1,10 +1,13 @@
 import { UserRepository } from './user-repository';
 import { TenantRepository } from './tenant-repository';
+import { IndustryRepository } from './industry-repository';
 
 const userRepository = new UserRepository();
 const tenantRepository = new TenantRepository();
+const industryRepository = new IndustryRepository();
 
 export {
   userRepository,
-  tenantRepository
+  tenantRepository,
+  industryRepository
 }
--- shared/src/common/enums/api/api-path.enum.ts
+++ shared/src/common/enums/api/api-path.enum.ts
@@ -2,7 +2,8 @@ enum ApiPath {
   USERS = '/users',
   TENANTS = '/tenants',
   AUTH = '/auth',
-  FILE_UPLOAD='/file-upload'
+  FILE_UPLOAD='/file-upload',
+  INDUSTRIES = '/industries',
 }
 
 export { ApiPath };
